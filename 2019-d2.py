b905e15f51c35fb7f47a006d1a35ad3d0fd178d3
Dino Budimilić <budidino@gmail.com>
2019-12-02 14:11:21 +0800

    2019 - day 2

---
+++ 2019-d2.py
@@ -0,0 +1,38 @@
+INPUT = "2019-d2.txt"
+array = open(INPUT).read().split(',')
+array = [int(i, base=10) for i in array]
+
+def process(arr, noun, verb):
+    arr[1] = noun
+    arr[2] = verb
+
+    pointer = 0
+    shouldStop = False
+
+    while not shouldStop:
+        opcode = arr[pointer]
+        param1 = arr[pointer+1]
+        param2 = arr[pointer+2]
+        param3 = arr[pointer+3]
+
+        if opcode == 1:
+            arr[param3] = arr[param1] + arr[param2]
+        elif opcode == 2:
+            arr[param3] = arr[param1] * arr[param2]
+        else:
+            shouldStop = True
+
+        pointer += 4
+
+    return arr
+
+# part 1
+part1 = process(array.copy(), 12, 2)
+print(f"part 1: {part1[0]}")
+
+# part 2
+for n in range(100):
+    for v in range(100):
+        part2 = process(array.copy(), n, v)
+        if part2[0] == 19690720:
+            print(f"part 2: {100 * n + v}")
