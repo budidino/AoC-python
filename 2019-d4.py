fa0541ae7e778054168283f77c9944d5c0f819a0
Dino Budimilić <budidino@gmail.com>
2019-12-04 14:12:06 +0800

    2019 - day 4

---
+++ 2019-d4.py
@@ -0,0 +1,32 @@
+start = 124075
+end = 580769
+
+def isAscending(number):
+    s = str(number)
+    return int(s[0]) <= int(s[1]) <= int(s[2]) <= int(s[3]) <= int(s[4]) <= int(s[5])
+
+def isRepeating(number):
+    string = str(number)
+    for char in string[1:-1]:
+        if string.count(char) >= 2:
+            return True
+    return False
+
+def isRepeatingTwice(number):
+    string = str(number)
+    for char in string[1:-1]:
+        if string.count(char) == 2:
+            return True
+    return False
+
+part1, part2 = 0, 0
+
+for i in range(start, end+1):
+    if isAscending(i):
+        if isRepeating(i):
+            part1 += 1
+            if isRepeatingTwice(i):
+                part2 += 1
+
+print("part 1:", part1)
+print("part 2:", part2)
