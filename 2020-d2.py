c72a0eb20dcd48e7de3849567be9cb0d246df0af
Dino Budimilic <budidino@gmail.com>
2020-12-03 00:12:05 +0100

    2020-d2 made it more readable

--- 2020-d2.py
+++ 2020-d2.py
@@ -2,17 +2,10 @@ INPUT = "2020-d2.txt"
 strings = [string.strip('\n') for string in open(INPUT)]
 
 part1, part2 = 0, 0
-
 for string in strings:
-    splits = string.split(" ")
-    letter = splits[1][0]
-    string = splits[2]
-    minMaxSplit = splits[0].split("-")
-    minVal = int(minMaxSplit[0])
-    maxVal = int(minMaxSplit[1])
-
-    part1 += minVal <= string.count(letter) <= maxVal
-    part2 += (string[minVal-1] == letter) != (string[maxVal-1] == letter)
-
-print(f"part 1: {part1}") # 548
-print(f"part 2: {part2}") # 502
+    policy, password = string.split(': ')
+    numbers, letter = policy.split(' ')
+    minVal, maxVal = map(int, numbers.split('-'))
+    part1 += minVal <= password.count(letter) <= maxVal
+    part2 += (password[minVal-1] == letter) != (password[maxVal-1] == letter)
+print(f"part 1: {part1}\npart 2: {part2}") # 548 # 502
